16c02748561299390091af05a84d45559ad5bfa3
Putra-XD <97390984+Putra-XD@users.noreply.github.com>
2022-01-11 21:49:43 +0700

    Update Ig-crk.py

--- Ig-crk.py
+++ Ig-crk.py
@@ -198,15 +198,15 @@ def logo():
 ### MENU ###
 def menu_dev():
 	logo()
-        print(" []---------------------PUTXD-------------------")
+	print(" [] Nomer WA    : 082223578245
 	print(" [] Bergabung   : %s"%(tgl))
 	print(" [] Github      : github.com/Putra-XD")
-	print(" [] Status      : %sPremium%s"% (H,N))
         print(" []---------------------------------------------")
+	print(" [] Status      : %sPremium%s"% (H,N))
 	print(" [] Kadaluwarsa : lifetime")
 	print(" [] Author      : putraexzy")
-	print(" [] IP User     : %s\n"%(IP))
         print(" []---------------------------------------------")
+	print(" [] IP User     : %s\n"%(IP))
 	print(" %sPREMIUM USER ACCOUNT%s \n"%(K,N))
 	print(" 1. crack dari akun publik")
 	print(" 2. crack dari pencarian")
